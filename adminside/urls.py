4388fce56d413bcf88bc8361757c7a184968996c
CenRIZZa <cenizamaki@gmail.com>
2024-08-25 10:32:43 +0800

    Interface csv ?

--- adminside/urls.py
+++ adminside/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
-    path('home/', views.adminPage),
+    path('home/', views.your_view),
     path('book-page-views/', views.book_page_views, name='book_page_views'),
     path('create-account/', views.create_account, name='create_account'),
     path('create-librarian/', views.create_librarian, name='create_librarian'),
