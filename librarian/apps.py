4a58fedff9ed959d68850573db4024508697dfb5
Pierre Avestruz <pierreavest@gmail.com>
2024-10-03 22:38:43 +0800

    librarian edit ui update

--- librarian/apps.py
+++ librarian/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class LibrarianConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'librarian'
-
-
-
-
-#masama ang mag away kaya itigil na natin to wahahahahaha
-#ano ang gagawin kapag walang kaldero ang sinampay dun sa loob ng pakwan ni tiyo
