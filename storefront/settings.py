534469abbb81be01464a54e622d659e65f55f660
Rizzgods <markryanceniza2002@gmail.com>
2024-10-09 23:50:48 +0800

    spinner lib

--- storefront/settings.py
+++ storefront/settings.py
@@ -167,11 +167,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+
 
 
 MEDIA_URL = '/media/'
