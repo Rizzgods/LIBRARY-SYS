c4b0800f3b697f1a97d012a100906b5259298fc2
Michael Pamulagan <pamulaganmichael947@gmail.com>
2024-11-04 11:59:03 +0800

    fixed search in student and changed author in upload librarian

--- librarian/forms.py
+++ librarian/forms.py
@@ -1,8 +1,29 @@
 from django import forms
 from .models import Books
-
+import json
 
 class BookForm(forms.ModelForm):
+    authors = forms.CharField(widget=forms.HiddenInput(), required=False)
+    Author = forms.CharField(required=False)
+
     class Meta:
         model = Books
-        fields = ['BookTitle', 'Author', 'Date', 'Language','Category','SubCategory', 'SubSection','BookFile', 'BookImage', 'Description', 'eBook','research_paper', 'hardCopy', 'stock'] 
+        fields = ['BookTitle', 'Author', 'Date', 'Language', 'Category', 'SubCategory', 'SubSection', 'BookFile', 'BookImage', 'Description', 'eBook', 'research_paper', 'hardCopy', 'stock']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        authors_json = cleaned_data.get('authors')
+        
+        try:
+            if authors_json:
+                authors_list = json.loads(authors_json)
+                if not authors_list:
+                    raise forms.ValidationError("At least one author is required.")
+                # Join authors with commas for storage in the Author field
+                cleaned_data['Author'] = ', '.join(authors_list)
+            else:
+                raise forms.ValidationError("At least one author is required.")
+        except json.JSONDecodeError:
+            raise forms.ValidationError("Invalid author data format.")
+        
+        return cleaned_data
