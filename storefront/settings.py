be00695fa05377dd4b17c90fa28aca59b2a6cdfe
Rizzgods <markryanceniza2002@gmail.com>
2024-12-11 20:11:29 +0000

    fix settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['128.199.98.54']
+ALLOWED_HOSTS = ['143.198.201.190']
 
 
 # Email settings for Gmail
